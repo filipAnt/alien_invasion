0bde4633bd5286c96cc1f4bf3e4f72d4149b5ab3
FilipAnt <filipantonczak92@gmail.com>
2021-06-13 17:48:51 +0200

    update ship steering

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,6 +4,7 @@ based on book Python Crash Course, 2nd Edition"""
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 
 
 class Alieninvasion:
@@ -13,25 +14,41 @@ class Alieninvasion:
         """create game resources"""
         pygame.init()
         self.settings = Settings()
-
         self.screen = pygame.display.set_mode((self.settings.screen_width,
                                                self.settings.screen_height))
         pygame.display.set_caption("Alien invasion")
+        self.ship = Ship(self)
 
     def run_game(self):
         """Main game loop"""
         while True:
-            # Wait unitl user will press Enter key or mouse button
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            # Refresh loop for every iteration
+            self._check_events()
+            self._update_screen()
 
             # Refresh game screen
             self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
 
             # Display last modified screen
             pygame.display.flip()
 
+    def _check_events(self):
+        """React on mouse and keyboard"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+
+    def _update_screen(self):
+        """Screen update"""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
 
 if __name__ == '__main__':
     # Run game
