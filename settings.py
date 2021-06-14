dbd3963cf04923afdd3ed1f0ebee1192a1882f3a
FilipAnt <filipantonczak92@gmail.com>
2021-06-14 20:55:09 +0200

    added score display

--- settings.py
+++ settings.py
@@ -28,6 +28,10 @@ class Settings:
 
         # Game speed settings
         self.speedup_scale = 1.1
+
+        # Points settings
+        self.score_scale = 1.5
+
         self.initialize_dynamic_settings()
 
     def initialize_dynamic_settings(self):
@@ -43,7 +47,10 @@ class Settings:
         self.alien_points = 50
 
     def increase_speed(self):
-        """Change speed settings"""
+        """Change speed and points settings"""
         self.ship_speed *= self.speedup_scale
         self.bullet_speed *= self.speedup_scale
         self.alien_speed *= self.speedup_scale
+
+        self.alien_points = int(self.alien_points * self.score_scale)
+        print(self.alien_points)
